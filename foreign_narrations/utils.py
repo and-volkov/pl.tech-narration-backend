b2c48bc6418f6a7db211be514957f396a9eb06c3
Андрей Волков <andrey.dreamwall@gmail.com>
2022-07-31 20:18:46 +0300

    create fill db script, refactor later

---
+++ foreign_narrations/utils.py
@@ -0,0 +1,34 @@
+import os
+from datetime import datetime as dt
+
+import audioread
+
+from database import shows_repository
+from models import Show, Narration
+
+
+DATA_DIR = '../data'
+
+sub_folders = [f.path for f in os.scandir(DATA_DIR) if f.is_dir()]
+
+for folder in sub_folders:
+    filenames = next(os.walk(folder), (None, None, []))[2]  # files
+    narrations = dict()
+    for file in filenames:
+        f = file.split('_')[-1]
+        language_tag = f.split('.')[0].strip()
+        file_extension = f.split('.')[1].strip()
+        file_path = os.path.abspath(folder) + '/' + file
+        file_size_in_mb = round(os.stat(file_path).st_size / 1024**2, 2)
+        with audioread.audio_open(file_path) as audio:
+            file_length_in_secs = round(audio.duration)
+        narrations[language_tag] = Narration(
+            file_name=file,
+            file_path=file_path,
+            file_size_in_mb=file_size_in_mb,
+            file_length_in_secs=file_length_in_secs,
+            file_extension=file_extension,
+            record_created=dt.today(),
+        )
+    record = Show(name=folder.split('/')[-1], narrations=narrations)
+    shows_repository.save(record)
