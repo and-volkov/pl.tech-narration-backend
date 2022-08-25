ca5f7ea3d7026231109585d73e996bc0df7e22e1
Andrey Volkov <andrey.dreamwall@gmail.com>
2022-08-25 14:38:13 +0300

    add cashe headers

--- foreign_narrations/app.py
+++ foreign_narrations/app.py
@@ -13,19 +13,19 @@ app = FastAPI(title=api_settings.title)
 
 
 origins = [
-    "*",
-    "http://10.10.120.140:3000",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
+    '*',
+    'http://10.10.120.140:3000',
+    'http://localhost',
+    'http://localhost:8080',
+    'http://localhost:3000',
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 
@@ -44,19 +44,19 @@ class ConnectionManager:
     async def broadcast(self, show: ShowHistory):
         for connection in self.active_connections:
             await asyncio.sleep(0.001)
-            await connection.send_json(show.json(), mode="text")
+            await connection.send_json(show.json(), mode='text')
 
 
 manager = ConnectionManager()
 
 
-@app.websocket("/api/ws")
+@app.websocket('/api/ws')
 async def send_show_notification(websocket: WebSocket):
     await manager.connect(websocket)
     try:
         while True:
             msg = await websocket.receive_text()
-            if msg.lower() == "close":
+            if msg.lower() == 'close':
                 await manager.disconnect(websocket)
                 await websocket.close()
     except WebSocketDisconnect:
@@ -65,8 +65,8 @@ async def send_show_notification(websocket: WebSocket):
 
 # TODO responses
 @app.post(
-    "/api/start/{show_name}",
-    responses={200: {"description": "Running show accepted"}},
+    '/api/start/{show_name}',
+    responses={200: {'description': 'Running show accepted'}},
 )
 async def get_start_command(show_name: str) -> Response:
     insert_new_running_show(show_name=show_name)
@@ -76,37 +76,49 @@ async def get_start_command(show_name: str) -> Response:
 
 # TODO Responses
 @app.get(
-    "/api/narrations", response_model=ShowHistory, response_class=Response
+    '/api/narrations',
+    response_model=ShowHistory,
+    response_class=Response,
+    responses={
+        200: {
+            'content': {'audio/mp3': {}},
+            'description': 'Return narration audio file',
+            'Cache-Control': 'max-age=10',
+            'Age': '10',
+        }
+    },
 )
 async def get_current_show():
     show = get_show()
     if show:
         return Response(show.json(), status_code=200)
-    return Response("No show running now", status_code=204)
+    return Response('No show running now', status_code=204)
 
 
 # TODO Responses
 @app.get(
-    "/api/narrations/{language_tag}",
+    '/api/narrations/{language_tag}',
     responses={
         200: {
-            "content": {"audio/mp3": {}},
-            "description": "Return narration audio file",
+            'content': {'audio/mp3': {}},
+            'description': 'Return narration audio file',
+            'Cache-Control': 'max-age=10',
+            'Age': '10',
         },
-        403: {"description": "Nothing to return. You die alone"},
+        403: {'description': 'Nothing to return. You die alone'},
     },
 )
 async def send_narration_file(
-    language_tag: str = "eng",
+    language_tag: str = 'eng',
 ) -> FileResponse | Response:
     file = get_show_narration(language_tag)
-    return FileResponse(file, media_type="audio/mp3")
+    return FileResponse(file, media_type='audio/mp3')
 
 
 # TODO Make run
-if __name__ == "__main__":
+if __name__ == '__main__':
     uvicorn.run(
-        "app:app",
+        'app:app',
         host=api_settings.host,
         port=api_settings.port,
         log_level=api_settings.log_level.lower(),
